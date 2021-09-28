7bd3b61602f210c6fa333c7e66befb932c26b40a
Никита Адамович <senior.nikita.adamovitch@mail.ru>
2021-09-28 08:55:01 +0500

    test edit

--- PZ5/Program.cs
+++ PZ5/Program.cs
@@ -6,7 +6,7 @@ namespace PZ5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello git!");
         }
     }
 }
