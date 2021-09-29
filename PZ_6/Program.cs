a3e7969f0ff93bdc1ba114848625784bffe83dbe
Никита Адамович <senior.nikita.adamovitch@mail.ru>
2021-09-29 12:58:25 +0500

    test edit

--- PZ_6/Program.cs
+++ PZ_6/Program.cs
@@ -6,7 +6,15 @@ namespace PZ_6
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите n: ");
+            int n;
+            n = int.Parse(Console.ReadLine());
+            int k = 1;
+            while (k*k < n)
+            {
+                Console.WriteLine(k * k);
+                k++;
+            }
         }
     }
 }
