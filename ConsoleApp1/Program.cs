ee8ed7e4070422bb92414c0368ff452c3c3ac2cb
Никита Адамович <senior.nikita.adamovitch@mail.ru>
2021-10-29 13:57:52 +0500

    test edit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] i = new int[10];
+            Random rnd = new Random();
+            int y = 0;
+            for (int j = 0; j < 10; j++)
+            {
+                i[j] = rnd.Next(10);
+                Console.Write(i[j] + " ");
+            }
+
+            for (int j = 0; j < 10; j++)
+            {
+                for (int k = 0; k < 10; k++)
+                {
+                    if(k!=j && i[j] > i[k])
+                    {
+                        y = i[j];
+                    }
+                }
+            }
+            Console.WriteLine(y);
+        }
+    }
+}
