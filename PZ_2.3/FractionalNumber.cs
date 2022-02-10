30e0d8a834a230ae9760670b19c507948d5630c5
Никита Адамович <senior.nikita.adamovitch@mail.ru>
2022-02-10 10:53:06 +0500

    test edit

---
+++ PZ_2.3/FractionalNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PZ_2._3
+{
+    class FractionalNumber
+    {
+        public char sign;
+        public int dividend;
+        public int divider;
+
+        public FractionalNumber(int n, int b)
+        {
+            dividend = n;
+            divider = b;
+        }
+        public FractionalNumber(char a, int n, int b)
+        {
+            sign = a;
+            dividend = n;
+            divider = b;
+        }
+        public FractionalNumber(int n)
+        {
+            dividend = n;
+        }
+        public FractionalNumber(char a, int n)
+        {
+            sign = a;
+            dividend = n;
+        }
+
+        public void Print()
+        {
+            if (divider != 0)
+                Console.WriteLine("Вы ввели число: " + sign + dividend + "/" + divider);
+            else
+                Console.WriteLine("Вы ввели число: " + sign + dividend);
+        }
+    }
+}
---
+++ PZ_2.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PZ_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FractionalNumber number1 = new FractionalNumber( '-', 24, 25);
+            number1.Print();
+            FractionalNumber number2 = new FractionalNumber(24, 25);
+            number2.Print();
+            FractionalNumber number3 = new FractionalNumber(24);
+            number3.Print();
+            FractionalNumber number4 = new FractionalNumber( '+', 24);
+            number4.Print();
+        }
+    }
+}
