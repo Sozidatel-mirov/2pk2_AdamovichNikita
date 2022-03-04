28c2c8dd390ee7cd6a9431de627e1d8d3ee58827
Никита Адамович <senior.nikita.adamovitch@mail.ru>
2022-03-04 20:57:20 +0500

    PZ_6

--- PZ_Calculatator/MainWindow.xaml.cs
+++ PZ_Calculatator/MainWindow.xaml.cs
@@ -18,105 +18,90 @@ namespace PZ_Calculatator
 {
     public partial class MainWindow : Window
     {
-        private static string expression;
         public MainWindow()
         {   
         }
         private void Button7_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += Button7.Content;
-            expression += Button7.Content;
         }
 
         private void Button8_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += Button8.Content;
-            expression += Button8.Content;
         }
 
         private void Button9_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += Button9.Content;
-            expression += Button9.Content;
         }
 
         private void ButtonPlus_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += ButtonPlus.Content;
-            expression += ButtonPlus.Content;
         }
 
         private void Button4_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += Button4.Content;
-            expression += Button4.Content;
         }
 
         private void Button5_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += Button5.Content;
-            expression += Button5.Content;
         }
 
         private void Button6_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += Button6.Content;
-            expression += Button6.Content;
         }
 
         private void ButtonMinus_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += ButtonMinus.Content;
-            expression += ButtonMinus.Content;
         }
 
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += Button1.Content;
-            expression += Button1.Content;
         }
 
         private void Button2_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += Button2.Content;
-            expression += Button2.Content;
         }
 
         private void Button3_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += Button3.Content;
-            expression += Button3.Content;
         }
 
         private void ButtonUmnojit_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += "*";
-            expression += "*";
         }
 
         private void Button0_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += Button0.Content;
-            expression += Button0.Content;
         }
 
         private void ButtonDelit_Click(object sender, RoutedEventArgs e)
         {
             AUG.Text += ButtonDelit.Content;
-            expression += ButtonDelit.Content;
         }
 
         private void ButtonRavno_Click(object sender, RoutedEventArgs e)
         {
             Regex r = new Regex(@"[\-\+\*\/]+");
-            Match tel = r.Match(expression);
+            Match tel = r.Match(AUG.Text);
             string znak = "0";
             while (tel.Success)
             {
                 znak = Convert.ToString(tel);
                 tel = tel.NextMatch();
             }
-            string text = expression;
+            string text = AUG.Text;
             string[] newText = Regex.Split(text, "[-+*/]+");
             if (znak == "+")
                 AUG.Text = $"{Convert.ToString(Convert.ToDouble(newText[0]) + Convert.ToDouble(newText[1]))}";
