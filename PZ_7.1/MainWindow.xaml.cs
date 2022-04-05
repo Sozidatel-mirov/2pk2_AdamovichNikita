89371a61c132a61677e71e8cf034ccb13f9f3b16
chelovek sverh <senior.nikita.adamovitch@ya.ru>
2022-04-06 02:57:20 +0500

    Редактор здесь

--- PZ_7.1/MainWindow.xaml.cs
+++ PZ_7.1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace PZ_7._1
 {
@@ -27,16 +28,41 @@ namespace PZ_7._1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (MainText.FontSize==20)
+            if (MainText.FontSize == 20)
                 MainText.FontSize = 25;
             else MainText.FontSize = 20;
         }
 
+        private void Click_New(object sender, RoutedEventArgs e)
+        {
+            CreateFileWindow createFileWindow = new CreateFileWindow();
+            if (createFileWindow.ShowDialog() == true)
+            {
+                File.Create(@"C:\Users\adamovich.nv1250\source\repos\PZ_7\PZ_7.1\Data\" + $"{createFileWindow.FileName}" +".txt");
+            }
+        }
+
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            if (MainText.FontStyle == "Italic")
-                FontStyle = "Italic";
+            if (MainText.FontSize == 20)
+                MainText.FontSize = 25;
             else MainText.FontSize = 20;
+        }
+
+        private void Click_Open(object sender, RoutedEventArgs e)
+        {
+            FileStream open = new FileStream(@"C:\Users\adamovich.nv1250\source\repos\PZ_7\PZ_7.1\Data\1.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader file = new StreamReader(open);
+            MainText.AppendText(file.ReadToEnd());
+            file.Close();
+        }
+
+        private void Click_Save(object sender, RoutedEventArgs e)
+        {
+            FileStream open = new FileStream(@"C:\Users\adamovich.nv1250\source\repos\PZ_7\PZ_7.1\Data\1.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(open);
+            writer.Write("22");
+            writer.Close();
         }
     }
 }
