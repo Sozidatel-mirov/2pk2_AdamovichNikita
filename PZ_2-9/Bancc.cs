ef9279f137d13d95f788e968ae4804a1ca86dc75
chelovek sverh <senior.nikita.adamovitch@ya.ru>
2022-04-20 18:28:11 +0500

    События

---
+++ PZ_2-9/Bancc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_2_9
+{
+    internal class Bancc
+    {
+        public int chet;
+        public Bancc(int ppp)
+        {
+            chet = ppp;
+        }
+        public void sniat(int sum)
+        {
+            chet -= sum;
+        }
+        public void vnesti(int sum)
+        {
+            chet += sum;
+        }
+    }
+}
---
+++ PZ_2-9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+delegate void MyDelegate();
+namespace PZ_2_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bancc chet = new Bancc(1000);
+            Client client = new Client(chet);
+
+            client.slejka();
+            Console.WriteLine();
+            Console.WriteLine("Снять 990");
+            chet.sniat(990);
+            client.slejka();
+            Console.WriteLine();
+            Console.WriteLine("Внести 1000");
+            chet.vnesti(1000);
+            client.slejka();
+        }
+    }
+}
